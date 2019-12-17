cd58abbed3949687dfffbc6e1ef59e15be019aab
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-12-17 16:37:01 +0530

    Added Bottom Sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
-import 'package:flutter/material.dart';
-import 'pages/homepage.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/homepage.dart';
 
 void main() => runApp(MaterialApp(
-  title: 'Hyperloop',
-  theme: ThemeData(
-    primarySwatch: Colors.blue,
-  ),
-  initialRoute: '/',
-  routes: {
-    '/': (context) => HomePage(),
-  },
-));
-
-
+      debugShowCheckedModeBanner: false,
+      title: 'Hyperloop',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: TextTheme(
+              body1:
+                  TextStyle(color: Colors.white, fontFamily: 'ProximaNova', fontSize: 20),)),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+      },
+    ));
